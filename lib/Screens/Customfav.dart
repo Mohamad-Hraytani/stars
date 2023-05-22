465aab71cb1e0ccf0141b9140fac21821e2086dc
Mohamad-Hraytani <m.mhraytani.job@gmail.com>
2023-05-22 23:17:29 +0300

    last update

--- lib/Screens/Customfav.dart
+++ lib/Screens/Customfav.dart
@@ -630,56 +630,6 @@ class _cucustomfavState extends State<customfav> {
   }
 }
 
-/* class profil1 extends CustomPainter {
-  Color c;
 
-  profil1(this.c);
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    print(c.toString());
-
-    Rect rect = Rect.fromLTWH(0, 0, 0, 0);
-
-    Gradient gradient = LinearGradient(
-      colors: [c, c],
-      stops: [0.5, 0.6],
-    );
-    Paint paint = Paint();
-    paint.shader = gradient.createShader(rect);
-
-    Path path = Path();
-
-    path.lineTo(size.width * 0.35, size.height * 0.35);
-
-    path.lineTo(size.width * 0.25, size.height * 0.38);
-    path.lineTo(size.width * 0.25, size.height * 0.25);
-
-    path.lineTo(size.width * 0.35, size.height * 0.15);
-
-    path.lineTo(size.width * 0.35, size.height * 0.75);
-    path.lineTo(size.width * 0.65, size.height * 0.75);
-
-    path.lineTo(size.width * 0.65, size.height * 0.35);
-    path.lineTo(size.width * 0.75, size.height * 0.38);
-    path.lineTo(size.width * 0.75, size.height * 0.25);
-
-    path.lineTo(size.width * 0.65, size.height * 0.15);
-    path.lineTo(size.width * 0.58, size.height * 0.15);
-
-    path.quadraticBezierTo(size.width * 0.50, size.height * 0.25,
-        size.width * 0.43, size.height * 0.15);
-
-    path.lineTo(size.width * 0.43, size.height * 0.15);
-    path.lineTo(size.width * 0.35, size.height * 0.15);
-    path.lineTo(size.width * 0.35, size.height * 0.35);
-
-    path.close();
-    canvas.drawPath(path, paint);
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
-} */
 
 
